5428e657039c0630a545f6d53fba21fba571ea6f
benjaminlyons <bencl1998@gmail.com>
2018-03-05 00:15:52 -0500

    My Chat Version

--- app.js
+++ app.js
@@ -14,19 +14,18 @@ var io = require('socket.io').listen(server);
 
 //when client connects, log it in the console
 io.sockets.on('connection', function(socket){
-	console.log('A client is connected!');
-	socket.emit('message', 'You are connected!');
-	socket.broadcast.emit('message', 'Another client has connected!');
 
 	//wait for client to send message to the server
-	socket.on('message', function(message){
-		console.log(socket.username + " is speaking to me! They are saying: " + message);
+	socket.on('newmessage', function(message){
+		console.log(socket.username + ' is sending a new message: ' + message);
+		socket.broadcast.emit('newmessage', {username: socket.username, message: message});
 	});
 
 	//wait for client username
 	socket.on('newuser', function(username){
 		socket.username = username;
-		console.log(socket.username);
+		console.log(socket.username + ' is now connected');
+		socket.broadcast.emit('newuser', socket.username);
 	});
 });
 
